d52b2f4bfb5540aae19a847bb1ffd0eeb98299b7
XP <smurfs@smurfs.verymad.net>
2025-06-14 13:19:29 -0700

    Fix ready check

--- src/main/groovy/app/xivgear/accountsvc/controllers/HealthReadyCheck.groovy
+++ src/main/groovy/app/xivgear/accountsvc/controllers/HealthReadyCheck.groovy
@@ -3,7 +3,10 @@ package app.xivgear.accountsvc.controllers
 import app.xivgear.accountsvc.nosql.RawNoSqlTable
 import groovy.transform.CompileStatic
 import groovy.util.logging.Slf4j
+import io.micronaut.context.BeanContext
+import io.micronaut.context.BeanProvider
 import io.micronaut.context.annotation.Context
+import io.micronaut.core.annotation.Order
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.HttpStatus
 import io.micronaut.http.MediaType
@@ -12,13 +15,20 @@ import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Produces
 import io.swagger.v3.oas.annotations.Operation
 import jakarta.annotation.security.PermitAll
+import jakarta.inject.Inject
+
 
 @Controller("/")
 @CompileStatic
 @Context
 @PermitAll
 @Slf4j
+@Order(-1000)
 class HealthReadyCheck {
+
+	@Inject
+	BeanContext ctx
+
 	@SuppressWarnings(['GrMethodMayBeStatic'])
 	@Operation(summary = "Health Check")
 	@Get("/healthz")
@@ -31,8 +41,12 @@ class HealthReadyCheck {
 	@Operation(summary = "Ready Check")
 	@Get("/readyz")
 	@Produces(MediaType.TEXT_PLAIN)
-	HttpResponse<String> readyCheck(List<RawNoSqlTable> tables) {
+	HttpResponse<String> readyCheck() {
+		// TODO: this doesn't really work as intended, because the http server doesn't start until
+		// all of the dependencies are already up
+		var tables = ctx.getBeansOfType RawNoSqlTable
 		tables.forEach {
+			log.info(it.tableName)
 			if (!it.initialized) {
 				log.info "Table ${it.tableName} not initialized"
 				return HttpResponse.status(HttpStatus.SERVICE_UNAVAILABLE)
